aa9bec5943806f870ba8549268a7bd525d78e153
YiyuZhang <brianazhang@berkeley.edu>
2021-06-10 12:23:34 -0700

    ed

--- lab3/src/main/java/problem1/NonPerishable.java
+++
@@ -1,8 +0,0 @@
-package problem1;
-
-public abstract class NonPerishable extends AbstractFood {
-
-  public NonPerishable(String name, double price, Integer available, Integer maxQuantity) {
-    super(name, price, available, maxQuantity);
-  }
-}
--- lab3/src/main/java/problem1/Rice.java
+++
@@ -1,8 +0,0 @@
-package problem1;
-
-public class Rice extends NonPerishable {
-
-  public Rice(String name, double price, Integer available, Integer maxQuantity) {
-    super(name, price, available, maxQuantity);
-  }
-}
